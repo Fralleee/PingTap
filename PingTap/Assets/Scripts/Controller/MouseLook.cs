133a044338623d4e5e86fb0a8e87e32dee3fe960
Roland Chelwing <jolleee@hotmail.com>
2020-01-31 10:20:56 +0100

    Added libraries
    Added libraries and setup PUN

--- PingTap/Assets/Scripts/Controller/MouseLook.cs
+++ PingTap/Assets/Scripts/Controller/MouseLook.cs
@@ -1,78 +1,56 @@
 using UnityEngine;
-using System.Collections;
 
-/// MouseLook rotates the transform based on the mouse delta.
-/// Minimum and Maximum values can be used to constrain the possible rotation
+public class MouseLook : MonoBehaviour
+{
 
-/// To make an FPS style character:
-/// - Create a capsule.
-/// - Add the MouseLook script to the capsule.
-///   -> Set the mouse look to use LookX. (You want to only turn character but not tilt it)
-/// - Add FPSInputController script to the capsule
-///   -> A CharacterMotor and a CharacterController component will be automatically added.
+  public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+  public RotationAxes axes = RotationAxes.MouseXAndY;
+  public float sensitivityX = 15F;
+  public float sensitivityY = 15F;
 
-/// - Create a camera. Make the camera a child of the capsule. Reset it's transform.
-/// - Add a MouseLook script to the camera.
-///   -> Set the mouse look to use LookY. (You want the camera to tilt up and down like a head. The character already turns.)
-[AddComponentMenu("Camera-Control/Mouse Look")]
-public class MouseLook : MonoBehaviour {
+  public float minimumX = -360F;
+  public float maximumX = 360F;
 
-	public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
-	public RotationAxes axes = RotationAxes.MouseXAndY;
-	public float sensitivityX = 15F;
-	public float sensitivityY = 15F;
+  public float minimumY = -60F;
+  public float maximumY = 60F;
 
-	public float minimumX = -360F;
-	public float maximumX = 360F;
+  public bool invertY = false;
 
-	public float minimumY = -60F;
-	public float maximumY = 60F;
+  float rotationY = 0F;
 
-	public bool invertY = false;
+  void Start()
+  {
+    Rigidbody rb = GetComponent<Rigidbody>();
+    if (rb) rb.freezeRotation = true;
+  }
 
-	float rotationY = 0F;
+  void Update()
+  {
+    float ySens = sensitivityY;
+    if (invertY) { ySens *= -1f; }
 
-	void Update ()
-	{
-		float ySens = sensitivityY;
-		if(invertY) { ySens *= -1f; }
-
-		if (axes == RotationAxes.MouseXAndY)
-		{
-			float rotationX = transform.localEulerAngles.y + GetMouseX() * sensitivityX;
-			
-			rotationY += GetMouseY() * ySens;
-			rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
-			
-			transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
-		}
-		else if (axes == RotationAxes.MouseX)
-		{
-			transform.Rotate(0, GetMouseX() * sensitivityX, 0);
-		}
-		else
-		{
-			rotationY += GetMouseY() * ySens;
-			rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
-			
-			transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
-		}
-	}
-	
-	void Start ()
-	{
-		// Make the rigid body not change rotation
-		if (GetComponent<Rigidbody>())
-			GetComponent<Rigidbody>().freezeRotation = true;
-	}
-
-    float GetMouseX()
+    if (axes == RotationAxes.MouseXAndY)
     {
-        return Input.GetAxis("Mouse X");
-    }
+      float rotationX = transform.localEulerAngles.y + GetMouseX() * sensitivityX;
 
-    float GetMouseY()
-    {
-        return Input.GetAxis("Mouse Y");
+      rotationY += GetMouseY() * ySens;
+      rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+      transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
     }
+    else if (axes == RotationAxes.MouseX)
+    {
+      transform.Rotate(0, GetMouseX() * sensitivityX, 0);
+    }
+    else
+    {
+      rotationY += GetMouseY() * ySens;
+      rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+      transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
+    }
+  }
+
+  float GetMouseX() => Input.GetAxis("Mouse X");
+  float GetMouseY() => Input.GetAxis("Mouse Y");
 }
