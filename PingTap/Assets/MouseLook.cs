9f4d7464f0811062d2117f891c27d392d33a149a
Roland Chelwing <jolleee@hotmail.com>
2020-02-22 12:38:16 +0100

    xd

---
+++ PingTap/Assets/MouseLook.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+  [Header("Mouse Look")] 
+  [SerializeField] GameObject orientation;
+  [SerializeField] float mouseSensitivity = 50f;
+  [SerializeField] float mouseLookSmooth = 0f;
+
+  [SerializeField] float clampYMax = 90f;
+  [SerializeField] float clampYMin = -90f;
+
+  [Header("Debug")]
+  [SerializeField] Vector3 inputMouseLook;
+  [SerializeField] float currentRotationX;
+  [SerializeField] float currentRotationY;
+
+  float mouseLookDampX;
+  float mouseLookDampY;
+  
+  void Awake()
+  {
+    Cursor.visible = false;
+    Cursor.lockState = CursorLockMode.Locked;
+  }
+
+  void Update()
+  {
+    GatherInputs();
+  }
+
+  void LateUpdate()
+  {
+    Look();
+  }
+
+  void GatherInputs()
+  {
+    inputMouseLook.x += Input.GetAxis("Mouse X") * mouseSensitivity * Time.fixedDeltaTime;
+    inputMouseLook.y -= Input.GetAxis("Mouse Y") * mouseSensitivity * Time.fixedDeltaTime;
+  }
+
+  void Look()
+  {
+    inputMouseLook.y = Mathf.Clamp(inputMouseLook.y, clampYMin, clampYMax);
+
+    currentRotationX = Mathf.SmoothDamp(currentRotationX, inputMouseLook.x, ref mouseLookDampX, mouseLookSmooth);
+    currentRotationY = Mathf.SmoothDamp(currentRotationY, inputMouseLook.y, ref mouseLookDampY, mouseLookSmooth);
+
+    orientation.transform.localRotation = Quaternion.Euler(0, currentRotationX, 0);
+    transform.localRotation = Quaternion.Euler(currentRotationY, currentRotationX, 0);
+  }
+
+
+}
--- PingTap/Assets/Scripts/Controller/PlayerController.cs
+++ PingTap/Assets/Scripts/Controller/PlayerController.cs
@@ -1,126 +1,86 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
   [Header("Movement")]
-  [SerializeField] float forwardSpeed = 40f;
-  [SerializeField] float strafeSpeed = 30f;
+  [SerializeField] float forwardSpeed = 100f;
+  [SerializeField] float strafeSpeed = 75f;
   [SerializeField] float maxSpeed = 5f;
-  [SerializeField] float stopTime = 0.1f;
-  [SerializeField] float jumpStrength = 4f;
-
-  [Header("Mouse Look")]
-  [SerializeField] float mouseSensitivity = 50f;
-  [SerializeField] float mouseLookSmooth = 0f;
+  [SerializeField] float stopTime = 0.05f;
+  [SerializeField] float jumpStrength = 8f;
 
   [Header("Debug")]
-  [SerializeField] Vector3 currentVelocity;
+  [SerializeField] string currentVelocity;
   [SerializeField] Vector3 inputMovement;
-  [SerializeField] Vector3 inputMouseLook;
-  [SerializeField] float currentRotationX;
-  [SerializeField] float currentRotationY;
 
   new Rigidbody rigidbody;
-  new Camera camera;
+  Transform orientation;
   bool performJump;
   float stopX;
   float stopZ;
-  float mouseLookDampX;
-  float mouseLookDampY;
   float distToGround;
 
-  bool isGrounded
-  {
-    get
-    {
-      return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f);
-    }
-  }
-
+  bool IsGrounded => Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f);
 
   void Awake()
   {
     rigidbody = GetComponent<Rigidbody>();
-    camera = GetComponentInChildren<Camera>();
     distToGround = GetComponent<Collider>().bounds.extents.y;
-
-
-    Cursor.visible = false;
-    Cursor.lockState = CursorLockMode.Locked;
+    orientation = transform.Find(("Orientation"));
   }
 
   void Update()
   {
     GatherInputs();
     Movement();
-    Jump();
   }
 
   void FixedUpdate()
   {
+    Jump();
     LimitSpeed();
     StoppingForces();
   }
 
-  void LateUpdate()
-  {
-    MouseLook();
-  }
-
   void GatherInputs()
   {
-    inputMovement.x = Input.GetAxis("Horizontal") * strafeSpeed;
-    inputMovement.y = Input.GetAxis("Vertical") * forwardSpeed;
-
-    inputMouseLook.x -= Input.GetAxis("Mouse Y") * mouseSensitivity * Time.fixedDeltaTime;
-    inputMouseLook.y += Input.GetAxis("Mouse X") * mouseSensitivity * Time.fixedDeltaTime;
-
-    performJump = Input.GetButtonDown("Jump");
+    inputMovement.x = Input.GetAxisRaw("Horizontal");
+    inputMovement.y = Input.GetAxisRaw("Vertical");
+    performJump = Input.GetButton("Jump");
   }
 
   void Movement()
   {
-    rigidbody.AddRelativeForce(inputMovement.x * Time.deltaTime, 0, inputMovement.y * Time.deltaTime, ForceMode.VelocityChange);
+    var modifier = IsGrounded ? 1 : 0.5f;
+    Vector3 force = orientation.right * inputMovement.x * strafeSpeed * Time.deltaTime + orientation.forward * inputMovement.y * forwardSpeed * Time.deltaTime;
+    rigidbody.AddRelativeForce(force * modifier, ForceMode.VelocityChange);
+    currentVelocity = new Vector3(rigidbody.velocity.x, 0, rigidbody.velocity.z).magnitude.ToString("##.0");
   }
 
   void LimitSpeed()
   {
     Vector3 horizontalMovement = new Vector3(rigidbody.velocity.x, 0, rigidbody.velocity.z);
-    if (horizontalMovement.magnitude > maxSpeed)
-    {
-      horizontalMovement = horizontalMovement.normalized * maxSpeed;
-      rigidbody.velocity = new Vector3(horizontalMovement.x, rigidbody.velocity.y, horizontalMovement.z);
-    }
+    if (!(horizontalMovement.magnitude > maxSpeed)) return;
+    horizontalMovement = horizontalMovement.normalized * maxSpeed;
+    rigidbody.velocity = new Vector3(horizontalMovement.x, rigidbody.velocity.y, horizontalMovement.z);
   }
 
   void StoppingForces()
   {
+    if (!IsGrounded || inputMovement.magnitude > 0.5f) return;
     float velocityX = Mathf.SmoothDamp(rigidbody.velocity.x, 0, ref stopX, stopTime);
     float velocityZ = Mathf.SmoothDamp(rigidbody.velocity.z, 0, ref stopZ, stopTime);
     rigidbody.velocity = new Vector3(velocityX, rigidbody.velocity.y, velocityZ);
   }
 
-  void MouseLook()
-  {
-    inputMouseLook.x = Mathf.Clamp(inputMouseLook.x, -90, 90);
-
-    currentRotationX = Mathf.SmoothDamp(currentRotationX, inputMouseLook.x, ref mouseLookDampX, mouseLookSmooth);
-    currentRotationY = Mathf.SmoothDamp(currentRotationY, inputMouseLook.y, ref mouseLookDampY, mouseLookSmooth);
-
-    camera.transform.localRotation = Quaternion.Euler(currentRotationX, 0, 0);
-    transform.localRotation = Quaternion.Euler(0, currentRotationY, 0);
-  }
-
   void Jump()
   {
-    if (performJump)
+    if (!performJump) return;
+    if (IsGrounded)
     {
-      performJump = false;
-      if (isGrounded)
-      {
-        rigidbody.AddForce(Vector3.up * jumpStrength, ForceMode.VelocityChange);
-      }
+      rigidbody.AddForce(Vector3.up * jumpStrength, ForceMode.VelocityChange);
     }
   }
 }
