6dad9db512a5b39d261e480a56a75c1c76f5b2f1
Roland Chelwing <jolleee@hotmail.com>
2020-03-04 11:17:50 +0100

    Level design
    Added lava
    Added some particles
    Fixed faces on other side of ground

---
+++ PingTap/Assets/Scripts/Core/Animation/AnimatedUVs.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public enum Axis
+{
+  X,
+  Y
+}
+
+public class AnimatedUVs : MonoBehaviour
+{
+  [SerializeField] float scrollSpeed = 0.25F;
+  [SerializeField] Axis direction;
+  new Renderer renderer;
+
+  void Start()
+  {
+    renderer = GetComponent<Renderer>();
+  }
+
+  void LateUpdate()
+  {
+    float offset = Time.time * scrollSpeed;
+    renderer.material.SetTextureOffset("_MainTex", new Vector2(direction == Axis.X ? offset : 0, direction == Axis.Y ? offset : 0));
+  }
+
+}
