620ba50de0067573b7b2b51a3f00f060698f1858
Roland Chelwing <jolleee@hotmail.com>
2020-07-14 15:40:46 +0200

    ass

---
+++ PingTap/Assets/Scripts/Core/Infrastructure/Pool.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+public partial class ObjectPooler
+{
+  [Serializable]
+  public class Pool
+  {
+    public string tag;
+    public GameObject prefab;
+    public int size;
+  }
+}
