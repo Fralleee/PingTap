8266d1e3666191c600ba80c0a66d18cf2ad15076
Roland Chelwing <jolleee@hotmail.com>
2020-12-15 16:19:09 +0100

    Fixed ammo segments
    Fixed ammo segments for:
    1 or 5+
    2
    3
    4

--- PingTap/Assets/Scripts/UI/HUD/AmmoCrosshairUI.cs
+++ PingTap/Assets/Scripts/UI/HUD/AmmoCrosshairUI.cs
@@ -8,14 +8,20 @@ namespace Fralle.UI.HUD
 {
 	public class AmmoCrosshairUI : MonoBehaviour
 	{
-		[SerializeField] Image image;
+		[SerializeField] Sprite oneSegment;
+		[SerializeField] Sprite twoSegments;
+		[SerializeField] Sprite threeSegments;
+		[SerializeField] Sprite fourSegments;
 
 		[SerializeField] int currentAmmo = 0;
 		[SerializeField] int maxAmmo = 0;
 
+		Image image;
+
 		void Awake()
 		{
 			image = GetComponent<Image>();
+
 			var combatant = GetComponentInParent<Combatant>();
 			combatant.OnWeaponSwitch += HandleWeaponSwitch;
 
@@ -31,7 +37,28 @@ namespace Fralle.UI.HUD
 			maxAmmo = ammoAddon.maxAmmo;
 			currentAmmo = ammoAddon.maxAmmo;
 
+			SetSpriteBasedOnAmmoCount(maxAmmo);
 			image.fillAmount = 1;
+		}
+
+		void SetSpriteBasedOnAmmoCount(int maxAmmo)
+		{
+			if (maxAmmo == 2)
+			{
+				image.sprite = twoSegments;
+			}
+			else if (maxAmmo == 3)
+			{
+				image.sprite = threeSegments;
+			}
+			else if (maxAmmo == 4)
+			{
+				image.sprite = fourSegments;
+			}
+			else
+			{
+				image.sprite = oneSegment;
+			}
 		}
 
 		void HandleAmmoChanged(int ammoCount)
