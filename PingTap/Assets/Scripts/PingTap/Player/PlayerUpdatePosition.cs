800fec800b8eb433c38db02faad8aa6dadeb9b81
Roland Chelwing <jolleee@hotmail.com>
2021-04-02 15:35:47 +0200

    Added LookAt IK

--- PingTap/Assets/Scripts/PingTap/Player/PlayerUpdatePosition.cs
+++ PingTap/Assets/Scripts/PingTap/Player/PlayerUpdatePosition.cs
@@ -1,17 +1,58 @@
-﻿using UnityEngine;
+﻿using Fralle.FpsController;
+using UnityEngine;
 
 namespace Fralle
 {
 	public class PlayerUpdatePosition : MonoBehaviour
 	{
+		[SerializeField] float rotateOnAngle = 35;
+
 		[SerializeField] Vector3 positionOffset;
 		[SerializeField] Transform body;
 		[SerializeField] Transform orientation;
 
+		PlayerController playerController;
+		bool doRotate;
+
+		void Awake()
+		{
+			playerController = GetComponentInParent<PlayerController>();
+		}
+
 		void Update()
 		{
 			transform.position = body.position + positionOffset;
-			transform.rotation = orientation.rotation;
+
+			UpdateRotation();
+		}
+
+		void UpdateRotation()
+		{
+
+
+
+			if (playerController.IsMoving)
+			{
+				transform.rotation = orientation.rotation;
+				doRotate = false;
+			}
+			else if (Vector3.Angle(orientation.forward, transform.forward) > rotateOnAngle)
+			{
+				doRotate = true;
+
+				// do rotate
+				//Quaternion.RotateTowards(transform.rotation, orientation.rotation, Time.deltaTime * 10f);
+
+				//transform.rotation = orientation.rotation;
+			}
+
+			if (doRotate)
+			{
+				Debug.Log("Rotate?");
+				transform.rotation = Quaternion.Lerp(transform.rotation, orientation.rotation, Time.deltaTime * 10f);
+				if (Vector3.Angle(orientation.forward, transform.forward) < 1f)
+					doRotate = false;
+			}
 		}
 	}
 }
