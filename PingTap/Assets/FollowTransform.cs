be1271f6621d1927597282c46b981e730f7faf36
Roland Chelwing <jolleee@hotmail.com>
2021-04-07 14:10:08 +0200

    Refactored model

---
+++ PingTap/Assets/FollowTransform.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class FollowTransform : MonoBehaviour
+{
+	[SerializeField] Transform transformToFollow;
+	[SerializeField] bool position;
+	[SerializeField] bool rotation;
+
+	void LateUpdate()
+	{
+		if (position)
+			transform.position = transformToFollow.position;
+		if (rotation)
+			transform.rotation = transformToFollow.rotation;
+	}
+}
